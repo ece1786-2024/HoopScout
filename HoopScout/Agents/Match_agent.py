204fb3e92f1bcd827be7d3f9f388ec537e828dae
Weiran Wang <wwr0129@gmail.com>
2024-11-17 16:41:04 -0500

    Restructure files, few agents updated

--- HoopScout/Agents/Match_agent.py
+++ HoopScout/Agents/Match_agent.py
@@ -2,7 +2,7 @@
 from openai import OpenAI
 import json
 import os
-client = OpenAI(api_key="API_KEY")
+client = OpenAI(api_key="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")
 
 prompt = """
 You are a professional basketball analyst tasked with finding players from a knowledge base who are most similar to a new NBA draft prospect based on their stats and background information.
@@ -19,16 +19,18 @@ def load_player_data(database_path):
     player_data = []
     for file in os.listdir(database_path):
         if file.endswith(".json"):
-            with open(os.path.join(database_path, file), 'r') as f:
+            with open(os.path.join(database_path, file), 'r', encoding='utf-8') as f:
                 player_data.append(json.load(f))
     return player_data
 
-def DE_agent_response(statement):
+def Match_agent_response(player_info, player_KB):
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "system", "content": prompt},
-            {"role": "user", "content": statement}
+            {"role": "user", "content": f"Given player's information{json.dumps(player_info)}" +
+             f"From the existing knowledge base, {player_KB}" +
+             "Please find the most similar players from the knowledge base to the given player if any."},
         ]
     )
 
@@ -36,4 +38,10 @@ def DE_agent_response(statement):
     return reply
 
 if __name__ == "__main__":
-    print("Unit test here")
+    with open('Input/Test.json', 'r', encoding='utf-8') as file:
+        player_info = json.load(file)
+        
+    player_KB = load_player_data('data/test')
+
+    result = Match_agent_response(player_info, player_KB)
+    print(result)
